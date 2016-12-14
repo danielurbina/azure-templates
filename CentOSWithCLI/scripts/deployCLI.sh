ab0d1e6dec330f1f7cb611c44e4f3b07fe4108a8
danielurbina <danielu@microsoft.com>
2016-12-14 02:51:01 +0100

    SU INSTEAD OF SUDO

--- CentOSWithCLI/scripts/deployCLI.sh
+++ CentOSWithCLI/scripts/deployCLI.sh
@@ -3,8 +3,8 @@ echo $(date) " - Starting Script"
 
 # Update system to latest packages and install dependencies
 echo $(date) " - Install base packages and update system to latest packages"
-echo 'Password1972'  | sudo -kS yum -y update --exclude=WALinuxAgent
-echo 'Password1972'  | sudo -kS yum -y install epel-release
-echo 'Password1972'  | sudo -kS yum -y install nodejs npm
-echo 'Password1972'  | sudo -kS npm install -g azure-cli
+su --session-command="yum -y update --exclude=WALinuxAgent"
+su --session-command="yum -y install epel-release"
+su --session-command="yum -y install nodejs npm"
+su --session-command="npm install -g azure-cli""
 
